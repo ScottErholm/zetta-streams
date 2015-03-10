d40b3e43064170bade55d25b40d707cad7fa96e5
Adam Magaluk <AdamMagaluK@gmail.com>
2015-03-10 13:29:40 -0400

    Consumer streams always subscribe to pubsub even when not reading, do to an update in v0.12.0

--- consumer_stream.js
+++ consumer_stream.js
@@ -5,19 +5,17 @@ var ConsumerStream = module.exports = function(queueName, options, pubsub) {
   Readable.call(this, options);
   this.queueName = queueName;
   this._pubsub = pubsub;
-  this.listener = null;
+  this._reading = true;
+
+  var self = this;
+  this._pubsub.subscribe(this.queueName, function(topic, data) {
+    if (self._reading && !self.push(data)) {
+      self._reading = false;
+    }
+  });
 };
 util.inherits(ConsumerStream, Readable);
 
 ConsumerStream.prototype._read = function(size) {
-  var self = this;
-  if (!this.listener) {
-    this.listener = function(topic, data) {
-      if (!self.push(data)) {
-        self.pubsub.unsubscribe(self.qeueueName, self.listener);
-        self.listener = null;
-      }
-    }
-    this._pubsub.subscribe(this.queueName, this.listener);
-  }
+  this._reading = true;
 };
